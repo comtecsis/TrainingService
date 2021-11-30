8db0c3c3d10280eb6dce1ac248c066c4c2367e14
ArtNL <arturofnl@outlook.com>
2021-11-30 09:09:15 -0500

    Agregando status en el enum

--- src/main/java/ws/synopsis/training/rest/enumeration/StatusEnum.java
+++ src/main/java/ws/synopsis/training/rest/enumeration/StatusEnum.java
@@ -8,7 +8,9 @@ public enum StatusEnum {
 
     OK("0000", "Proceso exitoso."),
     UNEXPECTED_ERROR("9000", "Error inesperado."),
-    FIELD_EMPTY("9001", "Campo est\u00E1 vac\u00EDo.");
+    FIELD_EMPTY("9001", "Campo est\u00E1 vac\u00EDo."),
+	ID_NOT_FOUND("9002","Id de cliente no encontrado"),
+	CELULAR_REGISTRADO("9003","El celular ya se encuentra registrado en el sistema");
 
     private final Status status;
 
