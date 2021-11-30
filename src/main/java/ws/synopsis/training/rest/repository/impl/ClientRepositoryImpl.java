60f8d16311020d22c10edbde0a744b3934046964
ArtNL <arturofnl@outlook.com>
2021-11-30 09:01:05 -0500

    Modificando los metodos add y update

--- src/main/java/ws/synopsis/training/rest/repository/impl/ClientRepositoryImpl.java
+++ src/main/java/ws/synopsis/training/rest/repository/impl/ClientRepositoryImpl.java
@@ -16,10 +16,10 @@ public class ClientRepositoryImpl implements ClientRepository {
 
     static {
         CLIENTS = new HashMap<Long, Client>();
-        CLIENTS.put(1L, Client.builder().idClient(1L).name("Elvis").lastName("Perez").build());
-        CLIENTS.put(2L, Client.builder().idClient(2L).name("Jhonatan").lastName("Evan\u00E0n").build());
-        CLIENTS.put(3L, Client.builder().idClient(3L).name("Arturo").lastName("Nu\u00F1ez").build());
-        CLIENTS.put(4L, Client.builder().idClient(4L).name("Luis").lastName("Quinto").build());
+        CLIENTS.put(1L, Client.builder().idClient(1L).name("Elvis").lastName("Perez").celular(993819382).build());
+        CLIENTS.put(2L, Client.builder().idClient(2L).name("Jhonatan").lastName("Evan\u00E0n").celular(983617383).build());
+        CLIENTS.put(3L, Client.builder().idClient(3L).name("Arturo").lastName("Nu\u00F1ez").celular(939137382).build());
+        CLIENTS.put(4L, Client.builder().idClient(4L).name("Luis").lastName("Quinto").celular(993133424).build());
     }
 
     @Override
@@ -28,17 +28,31 @@ public class ClientRepositoryImpl implements ClientRepository {
     }
 
     @Override
-    public void add(Client client) {
-        long id = (long) CLIENTS.size() + 1;
+    public boolean add(Client client) {
+        for(Map.Entry<Long, Client> m:CLIENTS.entrySet()) {
+        	if(m.getValue().getCelular() == client.getCelular()) {
+        		return true;
+        	}
+        }
+    	long id = (long) CLIENTS.size() + 1;
         client.setIdClient(id);
         CLIENTS.put(id, client);
+        return false;
     }
 
     @Override
-    public void update (Client client){
+    public boolean update (long clientId,Client client){
+    	boolean exists=CLIENTS.containsKey(clientId);
+    	
+    	if(!exists) {
+    		return false;
+    	}
+    	
         Client retrivedClient = CLIENTS.get(client.getIdClient());
             retrivedClient.setName(client.getName());
             retrivedClient.setLastName(client.getLastName());
+            retrivedClient.setCelular(client.getCelular());
+        return true;
     }
 
     @Override
